077fadb1240a05eeccbd9195de1764fc0e6d0bd5
Ian Felix <62842327+ianfelix@users.noreply.github.com>
2021-01-13 01:44:38 -0300

    Login created and login route too

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
+import Login from '../pages/authentication/Login';
 
 const MainRoute = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='login' element={<Login />} />
     </Routes>
   );
 };
