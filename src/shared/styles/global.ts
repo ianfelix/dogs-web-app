21d40caa74ae8071f68b8c6ed233a4ef99b2a5c4
Ian Felix <62842327+ianfelix@users.noreply.github.com>
2021-01-13 02:20:06 -0300

    feat: add more global styles

--- src/shared/styles/global.ts
+++ src/shared/styles/global.ts
@@ -12,4 +12,26 @@ export default createGlobalStyle`
     color: #333;
     font-family: ${typography.first}
   }
+
+  h1, h2,h3, h4, p{
+    margin: 0;
+  }
+
+  ul, li {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+  }
+
+  button, input {
+    display: block;
+    font-size: 1rem;
+    font-family: ${typography.first};
+    color: #333;
+  }
 `;
