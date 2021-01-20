1ee7035da15db10478f2bb2c07a6432073fddfd2
Ian Felix <62842327+ianfelix@users.noreply.github.com>
2021-01-20 20:37:47 -0300

    feat: Generics components button and input created

---
+++ src/shared/components/Forms/Button/Presentational.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+export const Presentational = () => {
+  return (
+    <Container>
+      <button>Enviar</button>
+    </Container>
+  );
+};
---
+++ src/shared/components/Forms/Input/Presentational.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+export const Presentational = () => {
+  return (
+    <Container>
+      <input type='text' />
+    </Container>
+  );
+};
